a478e7a3b2f764f9167ce7c372cd0d8d00c72c09
CarlosMac <carlosneira1997@gmail.com>
2019-02-02 17:47:14 -0500

    Socket IO user inout

--- LoL App/ViewController.swift
+++ LoL App/ViewController.swift
@@ -12,51 +12,55 @@ import SocketIO
 let manager = SocketManager(socketURL: URL(string: "https://af7af856.ngrok.io/")!, config: [.log(true), .compress])
 let socket = manager.defaultSocket
 
-class ViewController: UIViewController {
-    @IBOutlet var nameTextBox: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet var nameTextBox: UITextField!
     @IBOutlet var submitButton: UIButton!
     
-    var nameText  = "Mike";
     
-    func buttonCicked() {
-            
-    }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        nameTextBox.delegate = self
         nameTextBox.widthAnchor.constraint(equalToConstant: 200.0).isActive = true
         submitButton.layer.cornerRadius = 8
         submitButton.widthAnchor.constraint(equalToConstant: 150.0).isActive = true
         submitButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
-        //submitButton.addTarget(self, action: "buttonClicked", for: .touchUpInside)
+        
        
+        
         print("Loaded")
         socket.on(clientEvent: .connect) {data, ack in
             print("socket connected");
-            socket.emit("mobile-addPlayer", "Bob sagget");
-            
+            //socket.emit("mobile-addPlayer", "Bob sagget");
         }
         socket.on("ios") {data, ack in
             print("Conntect i guess");
-            
         }
         
-        
+         submitButton.addTarget(self, action:#selector (self.buttonClicked), for: .touchUpInside)
         
         
         socket.connect()
-
-        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    @objc func buttonClicked() {
+        let userName = String(nameTextBox.text!);
+        print("\(userName)");
+        socket.emit("mobile-addPlayer", "\(userName)");
+        
+    }
     
-    
-   
+    //UITextFieldDelegate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Return pressed")
+        nameTextBox.resignFirstResponder()
+        return true
+    }
     
 }
 
